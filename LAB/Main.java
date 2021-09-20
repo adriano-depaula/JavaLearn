79797e35fa6bcc87fc2f6674e766d9dd398114b9
Adriano de Paula <adriano-de.paula@outlook.com>
2021-09-20 08:15:02 -0300

    Mudança LAB

---
+++ LAB/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    // Declarando variável tipo string
+    String nome = "Adriano";
+    String sobrenome = "de Paula";
+    String nomeCompleto = nome + " " + sobrenome;
+        // Declarando variável inteira
+        int idade = 41;
+        // Declarando variável inteira
+        double minhaIdade = idade;
+        // Essa linha irá exibir a o texto entre aspas
+        System.out.println("Seu nome é " + nomeCompleto + " e você tem " + minhaIdade);
+    }
+}
